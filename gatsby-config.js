6585b51496c8fa61878a7bab3c9323e3f78dd345
yukicruz <yuki.cruz@gmail.com>
2024-08-02 03:25:05 -0700

    package.json add --prefix-paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/lifefocus', // replace with your repository name
+  pathPrefix: "/lifefocus", // replace with your repository name
   siteMetadata: {
     title: `Life Focus`,
     description: `Rediscover your core values, Redefine your life`,
@@ -22,14 +22,14 @@ module.exports = {
       options: {
         name: `Life Focus`,
         short_name: `LifeFocus`,
-        path: `${__dirname}`,
-        // start_url: `/`,
+        // path: `${__dirname}`,
+        start_url: `/lifefocus/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `${__dirname}/src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
